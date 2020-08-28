3aaa7127eeb662fc9b5ac1b6129704608b2da6c8
Flash <flash@top20talent.com>
2020-08-28 23:14:52 +0800

    add or perceptron numpy

--- deep-learning-from-scratch/chapter02-perceptron/test_practice.py
+++ deep-learning-from-scratch/chapter02-perceptron/test_practice.py
@@ -22,6 +22,10 @@ def nand_perceptron(x1, x2):
     return general_perceptron_np(x1, x2, w1=-0.5, w2=-0.5, b=0.7)
 
 
+def or_perceptron_np(x1, x2):
+    return general_perceptron_np(x1, x2, w1=0.5, w2=0.5, b=-0.2)
+
+
 class TestANDPerceptron:
     def test_0_0_to_0(self):
         assert and_perceptron(0, 0) == 0
@@ -60,3 +64,17 @@ class TestNANDPerceptron:
 
     def test_1_1_0(self):
         assert nand_perceptron(1, 1) == 0
+
+
+class TestORPerceptron:
+    def test_0_0_0(self):
+        assert or_perceptron_np(0, 0) == 0
+
+    def test_1_0_1(self):
+        assert or_perceptron_np(1, 0) == 1
+
+    def test_0_1_1(self):
+        assert or_perceptron_np(0, 1) == 1
+
+    def test_1_1_1(self):
+        assert or_perceptron_np(1, 1) == 1
