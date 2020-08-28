8f63e3423f83feb06814f4884330a13f72e1697a
Flash <flash@top20talent.com>
2020-08-28 22:45:42 +0800

    remove answer

--- deep-learning-from-scratch/chapter02-perceptron/test_practice.py
+++ deep-learning-from-scratch/chapter02-perceptron/test_practice.py
@@ -3,8 +3,8 @@ from practice import func
 
 def and_perceptron(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1 * w1 + x2 * w2
-    return 0 if tmp <= theta else 1
+    #  tmp = ?
+    # return 0 if tmp?theta else 1
 
 
 class TestANDPerceptron:
