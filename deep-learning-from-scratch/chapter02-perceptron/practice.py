35e54a457992c518274bbd13ba55f7464e80e685
Flash <flash@top20talent.com>
2020-08-28 23:22:11 +0800

    move

--- deep-learning-from-scratch/chapter02-perceptron/practice.py
+++ deep-learning-from-scratch/chapter02-perceptron/practice.py
@@ -1,2 +1,26 @@
-def func(x):
-    return x + 1
+import numpy as np
+
+
+def and_perceptron(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = None
+    return None
+
+
+def general_perceptron_np(x1, x2, w1, w2, b):
+    x = None
+    w = None
+    tmp = None
+    return None
+
+
+def and_perceptron_np(x1, x2):
+    return general_perceptron_np(x1, x2, w1=0.5, w2=0.5, b=-0.7)
+
+
+def nand_perceptron(x1, x2):
+    return general_perceptron_np(x1, x2, w1=-0.5, w2=-0.5, b=0.7)
+
+
+def or_perceptron_np(x1, x2):
+    return general_perceptron_np(x1, x2, w1=0.5, w2=0.5, b=-0.2)
--- deep-learning-from-scratch/chapter02-perceptron/test_practice.py
+++ deep-learning-from-scratch/chapter02-perceptron/test_practice.py
@@ -1,29 +1,5 @@
-import numpy as np
-
-
-def and_perceptron(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = None
-    return None
-
-
-def general_perceptron_np(x1, x2, w1, w2, b):
-    x = None
-    w = None
-    tmp = None
-    return None
-
-
-def and_perceptron_np(x1, x2):
-    return general_perceptron_np(x1, x2, w1=0.5, w2=0.5, b=-0.7)
-
-
-def nand_perceptron(x1, x2):
-    return general_perceptron_np(x1, x2, w1=-0.5, w2=-0.5, b=0.7)
-
-
-def or_perceptron_np(x1, x2):
-    return general_perceptron_np(x1, x2, w1=0.5, w2=0.5, b=-0.2)
+from practice import and_perceptron, and_perceptron_np, nand_perceptron, \
+    or_perceptron_np
 
 
 class TestANDPerceptron:
