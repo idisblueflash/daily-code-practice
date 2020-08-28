5813a4196aa2ee97b4545533a64ddb14a95e8534
Flash <flash@top20talent.com>
2020-08-28 22:43:47 +0800

    and perceptron

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # daily-code-practice
-Practice code related skill each day, and added daily
+Practice code related skill each day, and added daily,
+in a TDD way.
--- deep-learning-from-scratch/chapter02-perceptron/test_practice.py
+++ deep-learning-from-scratch/chapter02-perceptron/test_practice.py
@@ -1,5 +1,21 @@
 from practice import func
 
 
-def test_answer():
-    assert func(3) == 5
+def and_perceptron(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1 * w1 + x2 * w2
+    return 0 if tmp <= theta else 1
+
+
+class TestANDPerceptron:
+    def test_0_0_to_0(self):
+        assert and_perceptron(0, 0) == 0
+
+    def test_1_0_to_0(self):
+        assert and_perceptron(1, 0) == 0
+
+    def test_0_1_to_0(self):
+        assert and_perceptron(0, 1) == 0
+
+    def test_1_1_to_1(self):
+        assert and_perceptron(1, 1) == 1
