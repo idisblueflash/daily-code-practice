95570082ab94bc0491f205484251f06d85be7a1b
Flash <flash@top20talent.com>
2020-08-31 23:58:20 +0800

    remove answers

--- deep-learning-from-scratch/chapter_03.py
+++ deep-learning-from-scratch/chapter_03.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def step_function_original(x: np.array) -> np.array:
-    return x > 0
+    return None
 
 
 def step_function_int(x: np.array) -> np.array:
-    y = x > 0
-    return y.astype(np.int)
+    y = None
+    return None
