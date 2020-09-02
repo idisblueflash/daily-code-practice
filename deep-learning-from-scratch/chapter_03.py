73d8b5e827c04139c6aed3f10bffbc9c0f488c7c
Flash <flash@top20talent.com>
2020-09-02 19:27:25 +0800

    remove answers

--- deep-learning-from-scratch/chapter_03.py
+++ deep-learning-from-scratch/chapter_03.py
@@ -6,13 +6,13 @@ def step_function_original(x: np.array) -> np.array:
 
 
 def step_function_int(x: np.array) -> np.array:
-    y = x > 0
-    return y.astype(np.int)
+    y = None
+    return None
 
 
 def get_ranged_np_array(start: float, end: float, steps: float) -> np.array:
-    return np.arange(start, end, steps)
+    return None
 
 
 def sigmoid(x: np.array) -> np.array:
-    return 1 / (1 + np.exp(-x))
+    return None
