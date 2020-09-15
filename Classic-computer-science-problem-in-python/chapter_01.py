9a625e10080140fd537396128a573073b47f2bca
Flash <flash@top20talent.com>
2020-09-15 20:04:29 +0800

    remove answers

--- Classic-computer-science-problem-in-python/chapter_01.py
+++ Classic-computer-science-problem-in-python/chapter_01.py
@@ -1,15 +1,10 @@
 def fib1(n: int) -> int:
-    return fib1(n-1) + fib1(n-2)
+    raise NotImplementedError
 
 
 def fib2(n: int) -> int:
-    if n < 2:
-        return n
-    return fib2(n - 1) + fib2(n - 2)
+    raise NotImplementedError
 
 
 def fib25(n: int, counter: int) -> int:
-    if n < 2:
-        return counter
-    counter += 1
-    return fib2(n - 1) + fib2(n - 2)
+    raise NotImplementedError
