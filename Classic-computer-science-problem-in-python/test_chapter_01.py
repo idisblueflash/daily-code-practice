2ec2523af166432ff3239ce1b245d901731c7581
Flash <flash@top20talent.com>
2020-09-15 20:03:56 +0800

    refactor

--- Classic-computer-science-problem-in-python/test_chapter_01.py
+++ Classic-computer-science-problem-in-python/test_chapter_01.py
@@ -1,26 +1,11 @@
 import pytest
 
-
-def fib1(n: int) -> int:
-    return fib1(n-1) + fib1(n-2)
+from chapter_01 import fib1, fib2, fib25
 
 
 def test_fibonacci_infinite_loop():
     with pytest.raises(RecursionError):
         fib1(5)
-
-
-def fib2(n: int) -> int:
-    if n < 2:
-        return n
-    return fib2(n - 1) + fib2(n - 2)
-
-
-def fib25(n: int, counter: int) -> int:
-    if n < 2:
-        return counter
-    counter += 1
-    return fib2(n - 1) + fib2(n - 2)
 
 
 def test_fibonacci_without_exception():
